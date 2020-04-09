d8d0aa27d223f0c1a7d0639dd57fc732d1d68aee
Manjesh V <manjeshpv@gmail.com>
2020-04-09 09:06:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
+  res.writeHead(301,{ Location: process.env.REDIRECT_URL || 'https://github.com/manjeshpv/nodejs-docker/' });
+  res.end();
 }).listen(process.env.PORT || 80); //the server object listens on port 8080
